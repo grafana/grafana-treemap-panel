295b695134d82cfddf97ba81d0fe2c733d6ea2f6
Darren Janeczek <darren.janeczek@grafana.com>
2025-05-13 12:28:52 -0400

    fix: e2e test to match no-data case

--- tests/panel.spec.ts
+++ tests/panel.spec.ts
@@ -1,12 +1,12 @@
 import { test, expect } from '@grafana/plugin-e2e';
 
-test('should display "No data" in case panel data is empty', async ({
+test('should display "Configure your query" wizard in case panel data is empty', async ({
   gotoPanelEditPage,
   readProvisionedDashboard,
 }) => {
   const dashboard = await readProvisionedDashboard({ fileName: 'dashboard.json' });
   const panelEditPage = await gotoPanelEditPage({ dashboard, id: '2' });
-  await expect(panelEditPage.panel.locator).toContainText('No data');
+  await expect(panelEditPage.panel.locator).toContainText('Configure your query');
 });
 
 // TODO: Make equivalent tests for our panel
